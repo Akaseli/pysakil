8a9b0094f4011d72bdeafdc1b81a841925ef4abd
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2025-03-24 22:05:38 +0200

    Socketio and basic vehicle location updates

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,45 @@
 import express from 'express';
+import axios from 'axios';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server)
+
+
 
 app.get('/api', (req, res) => {
   res.send('Hello world.');
 });
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log("Connection received.");
+});
+
+server.listen(3000, () => {
   console.log('Backend up on port 3000');
 });
+
+
+//Vehicle updating
+const previousVehicles = new Map([]);
+
+
+setInterval(async () => {
+  //TODO add correct metadata
+  axios.get("https://data.foli.fi/siri/vm").then((response) => {
+    const vehicles = response.data["result"]["vehicles"];
+
+    Object.keys(vehicles).forEach((vehicle: string) => {
+      const pVec:any = previousVehicles.get(vehicle);
+      if(pVec != undefined){
+        if(pVec["recordedattime"] != vehicles[vehicle]["recordedattime"]){
+          io.to('vehicle-'+vehicle).emit("update", vehicles[vehicle])
+        }
+      }
+
+      previousVehicles.set(vehicle, vehicles[vehicle]);
+    });
+  })
+}, 3000)
