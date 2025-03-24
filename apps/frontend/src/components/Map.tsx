da10252648a6bc3e315b3b016814afaf97a18e37
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2025-03-24 14:38:13 +0200

    Cleanup.

--- apps/frontend/src/components/Map.tsx
+++ apps/frontend/src/components/Map.tsx
@@ -15,8 +15,45 @@ interface Props {
 }
 
 export const Map: React.FC<Props> = ({setActive}) => {
-  const map = useRef<L.Map>()
+  const icon = L.icon({ 
+    iconUrl: "/stop.svg",
+    iconSize: [24, 24],
+    iconAnchor: [12, 12],
+  })
 
+  const colors = ["#fc0303", "#fc6f03", "#fcd303", "#a1fc03", "#03fc41", "#03fcd7", "#0390fc", "#0320fc", "#0320fc", "#fc03e8", "#fc037f"]
+
+  const map = useRef<L.Map>()
+  const markerCluster = useRef<L.MarkerClusterGroup>()
+  const polyLines = useRef<L.LayerGroup>()
+
+  const activeMarker:L.Marker = new L.Marker([0, 0], {icon: icon});
+
+
+
+  const handleCurrent = (stop: number|null) => {
+    if(stop){
+      setActive(stop)
+      map.current?.addLayer(activeMarker);
+    }
+    else{
+      setActive(null)
+      map.current?.removeLayer(activeMarker);
+    }
+  }
+
+  const createClusterIcon = (cluster: L.MarkerCluster) => {
+    let number:number|string = cluster.getChildCount();
+    
+    if(number > 99){
+      number = "99+";
+    }
+
+    const html = '<div>' + number + '</div>';
+    return L.divIcon({html: html, className: "markerCluster", iconSize: L.point(36, 36)})
+  }
+
+  //Create map
   useEffect(() => {
     if(map.current) return;
     
@@ -32,50 +69,134 @@ export const Map: React.FC<Props> = ({setActive}) => {
       maxBoundsViscosity: 1.0
     })
 
+    map.current.on("click", () => {
+      handleCurrent(null)
+    })
+
+    //@ts-expect-error no types
     L.maplibreGL({
-      //@ts-expect-error no types
       style: "http://localhost:8080/styles/basic-preview/style.json"
     }).addTo(map.current);
-    
-    //Stops
-    const markers = L.markerClusterGroup({
+  });
+
+  //Create markerCluster
+  useEffect(() => {
+    if(markerCluster.current || !map.current) return;
+
+    markerCluster.current = L.markerClusterGroup({
       disableClusteringAtZoom: 16,
       spiderfyOnMaxZoom: false,
       polygonOptions: {
         color: "#e3b945"
       },
-      iconCreateFunction: (cluster) => {
-        let number:number|string = cluster.getChildCount();
-        
-        if(number > 99){
-          number = "99+";
-        }
-
-        const html = '<div>' + number + '</div>';
-        return L.divIcon({html: html, className: "markerCluster", iconSize: L.point(36, 36)})
-      }
+      iconCreateFunction: createClusterIcon
     });
 
-    const pLines = new L.LayerGroup();
-
-    const colors = ["#fc0303", "#fc6f03", "#fcd303", "#a1fc03", "#03fc41", "#03fcd7", "#0390fc", "#0320fc", "#0320fc", "#fc03e8", "#fc037f"]
-
-    
-
-    const icon = L.icon({ 
-      iconUrl: "/stop.svg",
-      iconSize: [24, 24],
-      iconAnchor: [12, 12],
-    })
-
-    const activeMarker = new L.Marker([0, 0], {icon: icon});
-    
     map.current.on("click", () => {
-      setActive(null);
-      map.current?.addLayer(markers);
-      map.current?.removeLayer(activeMarker);
-      map.current?.removeLayer(pLines);
+      if(markerCluster.current){
+        map.current?.addLayer(markerCluster.current);
+      }
     })
+  
+  });
+
+  //Create polylines
+  useEffect(() => {
+    if(polyLines.current ||!map.current) return;
+
+    polyLines.current = new L.LayerGroup();
+
+    map.current.on("click", () => {
+      if(polyLines.current){
+        map.current?.removeLayer(polyLines.current)
+      }
+    })
+  });
+
+  const handleMarkerClick = (stop_id: number, marker: L.Marker) => {
+    activeMarker.setLatLng(marker.getLatLng())
+    
+    //Clear map
+    if(markerCluster.current){
+      map.current?.removeLayer(markerCluster.current);
+    }
+    polyLines.current?.clearLayers();
+
+    //Update sidebar
+    handleCurrent(stop_id);
+
+    //Polylines
+    axios.get("https://data.foli.fi/siri/sm/" + stop_id).then((response) => {
+      if(!response.data) return;
+
+      const data = response.data["result"] as VehicleData[];
+      //pick different route refs
+      const lifeRefs:string[] = [];
+      const destinations: string[] = [];
+      const shapeIds: string[] = [];
+
+      data.forEach((vehicle) => {
+        if(!lifeRefs.includes(vehicle.lineref)){
+          lifeRefs.push(vehicle.lineref);
+          destinations.push(vehicle.destinationdisplay);
+        }
+      })
+
+      axios.get("https://data.foli.fi/gtfs/routes").then((response) => {
+        if(!response.data) return;
+
+        const routeData = response.data as RouteData[];
+
+        const routeIds:string[] = [];
+
+        routeData.forEach((route) => {
+          if(lifeRefs.includes(route.route_short_name)){
+            routeIds.push(route.route_id);
+          }
+        })
+
+        routeIds.forEach((id) => {
+          axios.get("http://data.foli.fi/gtfs/trips/route/" + id).then((response) => {
+            if(!response.data) return;
+
+            const tripData = response.data as TripData[];
+            
+            tripData.forEach((trip) => {
+              if(!shapeIds.includes(trip.shape_id) && destinations.includes(trip.trip_headsign)){
+                shapeIds.push(trip.shape_id);
+                
+                let shape: Shape[] = [];
+
+                axios.get("https://data.foli.fi/gtfs/shapes/" + trip.shape_id).then((response) => {
+                  if(!response.data) return;
+
+                  shape = response.data as Shape[];
+                })
+                .then(() =>  {
+                  const points:L.LatLng[] = []
+
+                  shape.forEach((point) => {
+                    points.push(new L.LatLng(point.lat, point.lon))
+                  });
+
+                  const polyline = L.polyline(points, {color: colors[shape[shape.length-1].traveled % colors.length]});
+                  polyline.bringToBack()
+                  polyLines.current?.addLayer(polyline);
+                })
+              }
+            })
+          })
+        })
+      })
+    });
+    
+    if(polyLines.current){
+      map.current?.addLayer(polyLines.current);
+    }
+  }
+
+  useEffect(() => {
+    if(!map.current || !markerCluster.current) return;
 
     axios.get("http://data.foli.fi/gtfs/stops").then((response) => {
       Object.entries(response.data).forEach(([key, value]) => {
@@ -84,94 +205,14 @@ export const Map: React.FC<Props> = ({setActive}) => {
         if(map.current){
           const marker = L.marker([stop.stop_lat, stop.stop_lon], {icon: icon});
           //On marker open
-          marker.on("click", () => {
-            activeMarker.setLatLng(marker.getLatLng());
-            map.current?.addLayer(activeMarker);
-            map.current?.removeLayer(markers);
-
-            setActive(stop.stop_code);
-            pLines.clearLayers();
-
-            //Polylines
-            axios.get("https://data.foli.fi/siri/sm/" + stop.stop_code).then((response) => {
-              if(!response.data) return;
-
-              const data = response.data["result"] as VehicleData[];
-              //pick different route refs
-              const lifeRefs:string[] = [];
-              const destinations: string[] = [];
-              const shapeIds: string[] = [];
-
-              data.forEach((vehicle) => {
-                if(!lifeRefs.includes(vehicle.lineref)){
-                  lifeRefs.push(vehicle.lineref);
-                  destinations.push(vehicle.destinationdisplay);
-                }
-              })
-
-              axios.get("https://data.foli.fi/gtfs/routes").then((response) => {
-                if(!response.data) return;
-
-                const routeData = response.data as RouteData[];
-
-                const routeIds:string[] = [];
-
-                routeData.forEach((route) => {
-                  if(lifeRefs.includes(route.route_short_name)){
-                    routeIds.push(route.route_id);
-                  }
-                })
-
-                routeIds.forEach((id) => {
-                  axios.get("http://data.foli.fi/gtfs/trips/route/" + id).then((response) => {
-                    if(!response.data) return;
-
-                    const tripData = response.data as TripData[];
-                    
-                    tripData.forEach((trip) => {
-                      if(!shapeIds.includes(trip.shape_id) && destinations.includes(trip.trip_headsign)){
-                        shapeIds.push(trip.shape_id);
-                        
-                        let shape: Shape[] = [];
-
-                        axios.get("https://data.foli.fi/gtfs/shapes/" + trip.shape_id).then((response) => {
-                          if(!response.data) return;
-
-                          shape = response.data as Shape[];
-                        })
-                        .then(() =>  {
-                          const points:L.LatLng[] = []
-
-                          shape.forEach((point) => {
-                            points.push(new L.LatLng(point.lat, point.lon))
-                          });
-
-                          const polyline = L.polyline(points, {color: colors[shape[shape.length-1].traveled % colors.length]});
-                          polyline.bringToBack()
-                          pLines.addLayer(polyline);
-                        })
-                      }
-                    })
-                  })
-                })
-              })
-
-              
-              
-              
-            });
-
-            map.current?.addLayer(pLines);
-          })
-          markers.addLayer(marker);
+          marker.on("click", () => handleMarkerClick(stop.stop_code, marker))
+          markerCluster.current?.addLayer(marker);
         }
         
       }); 
     })
-    map.current.addLayer(markers);
+    map.current.addLayer(markerCluster.current);
   }, [])
- 
-  
 
   return(
     <div>
