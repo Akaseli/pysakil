87535706fab7e40b4f05ce01e1d1167f27aea289
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2025-03-26 21:44:10 +0200

    Improved sidebar ui to display which vehicle is tracked.

--- apps/frontend/src/components/Sidebar.tsx
+++ apps/frontend/src/components/Sidebar.tsx
@@ -10,6 +10,7 @@ interface Props {
 export const Sidebar: React.FC<Props> = ({stop, setVehicle}) => {
   const [vehicles, setVehicles] = useState<VehicleData[]>([]);
   const [stopData ,setStopData] = useState<Stop>();
+  const [visibleVehicle, setVisibleVehicle] = useState<string|null>(null);
 
   useEffect(() => {
     axios.get("https://data.foli.fi/siri/sm/" + stop).then((response) => {
@@ -25,12 +26,22 @@ export const Sidebar: React.FC<Props> = ({stop, setVehicle}) => {
     });
   }, [stop])
 
+  const handleVehicle = (tripref: string) => {
+    setVehicle(tripref)
+    setVisibleVehicle(tripref)
+  }
+
   const vehicleCards = vehicles.map((vehicle) => {
     const timeToArrival = Math.abs(Math.round((vehicle.aimedarrivaltime - Date.now()/1000) / 60));
 
 
-    return <div className='vehicleCard' onClick={() => {setVehicle(vehicle.__tripref)}}>
+    return <div className='vehicleCard' onClick={() => {handleVehicle(vehicle.__tripref)}}>
       <p>{vehicle.lineref + " - " + vehicle.destinationdisplay}<span className="estimate">{timeToArrival + "min"}</span></p>
+      {
+        vehicle.monitored ? ( 
+          (vehicle.__tripref == visibleVehicle) ? <div className='tracked active'/> : <div className='tracked'/> 
+      ) : <div />
+      }
     </div>
   })
 
